8f0fce180e908550b0e12dbcf1cfecdff5f139e7
liguiqin <xinaml@qq.com>
2017-03-02 14:17:02 +0800

    删除japi

--- demo/demo-card/demo-card-consumer/src/main/java/com/bjike/goddess/card/Application.java
+++ demo/demo-card/demo-card-consumer/src/main/java/com/bjike/goddess/card/Application.java
@@ -1,11 +1,11 @@
 package com.bjike.goddess.card;
 
-import com.dounine.japi.ApplicationClient;
-import com.dounine.japi.utils.FileUtil;
-import org.apache.http.client.methods.HttpPost;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ImportResource;
 
 import java.io.IOException;
 
@@ -16,15 +16,15 @@ import java.io.IOException;
                 value = {Configuration.class})})
 @ImportResource("classpath:app.xml")
 public class Application{
-
-	public static void initJapi(){
-		try {
-			FileUtil.UPLOAD_REQUEST = new HttpPost("http://localhost:8080/interfaceapidoc/upload");
-			ApplicationClient.startClient("demo-card-consumer","/home/ike/java/github/goddess-java/demo/demo-card/demo-card-consumer/src/main/java/com/bjike/goddess/card/action","com.bjike.goddess.card.action",Application.class);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+//
+//	public static void initJapi(){
+//		try {
+//			FileUtil.UPLOAD_REQUEST = new HttpPost("http://localhost:8080/interfaceapidoc/upload");
+//			ApplicationClient.startClient("demo-card-consumer","/home/ike/java/github/goddess-java/demo/demo-card/demo-card-consumer/src/main/java/com/bjike/goddess/card/action","com.bjike.goddess.card.action",Application.class);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 
 
 
