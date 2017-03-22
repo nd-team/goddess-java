4f6ac19fb6618c635b4c4d83db52f2b7b8deb28b
server <102535481@qq.com>
2017-03-22 16:25:05 +0800

    update jar.sh

--- scripts/jar.sh
+++ scripts/jar.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd .. && /soft/gradle/bin/gradle jar -P$1 -xtest && /soft/gradle/bin/gradle jar -P$1 -xtest
+cd /storage/dockers/commons/goddess-java && /soft/gradle/bin/gradle jar -P$1 -xtest && /soft/gradle/bin/gradle jar -P$1 -xtest
