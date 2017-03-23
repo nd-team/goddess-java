822a0b8341a40896e98f117fa831e21b9b6d43e0
thx <804322698@qq.com>
2017-03-23 09:06:15 +0800

    增加商务项目合同

---
+++ modules/businessproject/businessproject-api/src/main/java/com/bjike/goddess/businessproject/enums/ContractProperty.java
@@ -0,0 +1,56 @@
+package com.bjike.goddess.businessproject.enums;
+
+/**
+ * 合同属性
+ *
+ * @Author: [tanghaixiang]
+ * @Date: [2017-03-20 19:57]
+ * @Description: [合同属性]
+ * @Version: [1.0.0]
+ * @Copy: [com.bjike]
+ */
+public enum ContractProperty {
+
+    /**
+     * 框架合同
+     */
+    FRAMECONTRACT(0),
+    /**
+     * 单次合同
+     */
+    SINGLECONTRACT(1);
+
+    private int code;
+
+    ContractProperty(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public static ContractProperty getEnumConvert(int code) {
+        ContractProperty contractProperty = ContractProperty.SINGLECONTRACT;
+        if (code == ContractProperty.FRAMECONTRACT.getCode()) {
+            contractProperty = ContractProperty.FRAMECONTRACT;
+        }
+        if (code == ContractProperty.SINGLECONTRACT.getCode()) {
+            contractProperty = ContractProperty.SINGLECONTRACT;
+        }
+        return contractProperty;
+    }
+
+    public static String getStrConvert(int code) {
+        String name = "";
+        if (code == ContractProperty.FRAMECONTRACT.getCode()) {
+            name = "框架合同";
+        }
+        if (code == ContractProperty.SINGLECONTRACT.getCode()) {
+            name = "单次合同";
+        }
+        return name;
+    }
+}
+
+
