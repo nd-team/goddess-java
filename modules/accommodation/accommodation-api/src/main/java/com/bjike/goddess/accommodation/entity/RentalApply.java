cfab9e30dbf5f39c7aed98f55a36ac764359be24
liguiqin <xinaml@qq.com>
2017-03-25 10:54:50 +0800

    导入缺少包

--- modules/accommodation/accommodation-api/src/main/java/com/bjike/goddess/accommodation/entity/RentalApply.java
+++ modules/accommodation/accommodation-api/src/main/java/com/bjike/goddess/accommodation/entity/RentalApply.java
@@ -2,6 +2,7 @@ package com.bjike.goddess.accommodation.entity;
 
 import com.bjike.goddess.common.api.entity.BaseEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
