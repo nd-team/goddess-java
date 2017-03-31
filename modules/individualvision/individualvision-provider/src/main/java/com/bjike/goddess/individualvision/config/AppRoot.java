c4c4655b8b1b477d5dce88029f8fd686516746d1
ike <ike@localhost.localdomain>
2017-03-31 18:22:52 +0800

    回款管理，个人愿景模块

---
+++ modules/individualvision/individualvision-provider/src/main/java/com/bjike/goddess/individualvision/config/AppRoot.java
@@ -0,0 +1,20 @@
+package com.bjike.goddess.receivable.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.*;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = {"com.bjike.goddess.receivable.dao"})//jpa 所在包
+@EnableTransactionManagement(proxyTargetClass = true)
+@EnableCaching
+@PropertySource({"classpath:config.properties"})
+@ImportResource({"classpath:application.xml"})
+@ComponentScan(basePackages = {"com.bjike.goddess.receivable"},
+        excludeFilters = {@ComponentScan.Filter(
+                type = FilterType.ANNOTATION,
+                value = {Configuration.class})})
+public class AppRoot {
+
+}
