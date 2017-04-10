5743f532496962019959674597ad4974ef417508
ike <ike@localhost.localdomain>
2017-04-10 11:56:04 +0800

    住宿管理

--- modules/accommodation/accommodation-provider/src/main/java/com/bjike/goddess/accommodation/Application.java
+++ modules/accommodation/accommodation-provider/src/main/java/com/bjike/goddess/accommodation/Application.java
@@ -1,11 +1,22 @@
+package com.bjike.goddess.accommodation;
+
 import com.bjike.goddess.accommodation.config.AppRoot;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
 
+/**
+ * 用户通用模块发布远程调用接口
+ *
+ * @Author: [liguiqin]
+ * @Date: [2016-11-23 15:47]
+ * @Description: []
+ * @Version: [1.0.0]
+ * @Copy: [com.bjike]
+ */
 public class Application {
-	
-	static AnnotationConfigApplicationContext context = null;
+
+    static AnnotationConfigApplicationContext context = null;
 
     public static void main(String[] args) throws IOException {
         context = new AnnotationConfigApplicationContext(AppRoot.class);
