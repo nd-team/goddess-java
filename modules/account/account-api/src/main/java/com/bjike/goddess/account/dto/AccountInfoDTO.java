7d59ae7ffdd2e61ccfebe672d5c9fbaa487d3f21
ike <ike@localhost.localdomain>
2017-04-10 08:39:59 +0800

    明细账模块

---
+++ modules/account/account-api/src/main/java/com/bjike/goddess/account/dto/AccountInfoDTO.java
@@ -0,0 +1,16 @@
+package com.bjike.goddess.account.dto;
+
+import com.bjike.goddess.common.api.dto.BaseDTO;
+
+/**
+ * 明细账信息数据传输对象
+ *
+ * @Author: [ xiazhili ]
+ * @Date: [ 2017-04-06 11:25 ]
+ * @Description: [ 明细账信息数据传输对象 ]
+ * @Version: [ v1.0.0 ]
+ * @Copy: [ com.bjike ]
+ */
+public class AccountInfoDTO extends BaseDTO {
+
+}
