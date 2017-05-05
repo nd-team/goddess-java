c419ec14931df7b392380c614928c9e28047cc99
root <jason4job@126.com>
2017-05-05 15:11:09 +0800

    竞争对手修改

--- modules/competitormanage/competitormanage-consumer/src/main/java/com/bjike/goddess/competitormanage/config/CustomIntercept.java
+++ modules/competitormanage/competitormanage-consumer/src/main/java/com/bjike/goddess/competitormanage/config/CustomIntercept.java
@@ -2,7 +2,13 @@ package com.bjike.goddess.competitormanage.config;
 
 import com.bjike.goddess.common.consumer.config.HIInfo;
 import com.bjike.goddess.common.consumer.config.Interceptor;
+import com.bjike.goddess.common.consumer.interceptor.auth.AuthIntercept;
+import com.bjike.goddess.common.consumer.interceptor.limit.SmoothBurstyInterceptor;
+import com.bjike.goddess.common.consumer.interceptor.login.LoginIntercept;
 import com.bjike.goddess.common.consumer.interceptor.login.StorageIntercept;
+import com.bjike.goddess.user.api.UserAPI;
+import com.bjike.goddess.user.api.rbac.PermissionAPI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -19,8 +25,39 @@ import java.util.List;
 public class CustomIntercept implements Interceptor {
 
 
+    @Autowired
+    private UserAPI userAPI;
+    @Autowired
+    private PermissionAPI permissionAPI;
+
     @Override
     public List<HIInfo> customerInterceptors() {
-        return Arrays.asList(new HIInfo(new StorageIntercept(), "/**"));
+        /**
+         * 添加限流器
+         */
+        SmoothBurstyInterceptor smoothInterceptor = new SmoothBurstyInterceptor(100, SmoothBurstyInterceptor.LimitType.DROP);
+        HIInfo smoothInfo = new HIInfo(smoothInterceptor, "/**");
+
+        /**
+         * 登录拦截器
+         */
+        HIInfo loginInfo = new HIInfo(new LoginIntercept(userAPI), "/**");
+
+        /**
+         * 权限拦截器
+         */
+        String[] excludes = new String[]{
+                "*/login",
+                "*/register",
+                "/user/version/verifyPhone/*",
+                "/user/version/register/*",
+                "public/version/key"
+        };
+        HIInfo authInfo = new HIInfo(new AuthIntercept(permissionAPI, excludes), "/**");
+
+        /**
+         * 暂时不加权限
+         */
+        return Arrays.asList(smoothInfo, loginInfo);
     }
 }
