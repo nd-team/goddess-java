c482511e1eeda440e7aec6370c5167aace0d22f2
liguiqin <xinaml@qq.com>
2017-05-17 10:09:28 +0800

    优化模块,添加执行sql接口

--- modules/assemble/assemble-provider/src/main/java/com/bjike/goddess/assemble/service/ModuleSerImpl.java
+++ modules/assemble/assemble-provider/src/main/java/com/bjike/goddess/assemble/service/ModuleSerImpl.java
@@ -12,10 +12,13 @@ import com.bjike.goddess.common.api.dto.Restrict;
 import com.bjike.goddess.common.api.exception.SerException;
 import com.bjike.goddess.common.jpa.service.ServiceImpl;
 import com.bjike.goddess.common.utils.bean.BeanTransform;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -36,6 +39,7 @@ public class ModuleSerImpl extends ServiceImpl<Module, ModuleDTO> implements Mod
 
     @Override
     public List<ModuleBO> list(ModuleDTO moduleDTO) throws SerException {
+        moduleDTO.setSorts(Arrays.asList("createTime=asc"));
         return BeanTransform.copyProperties(super.findByCis(moduleDTO), ModuleBO.class);
     }
 
@@ -52,25 +56,26 @@ public class ModuleSerImpl extends ServiceImpl<Module, ModuleDTO> implements Mod
     }
 
     @Override
-    public ModuleBO modulesByName(String name,CheckType checkType) throws SerException {
+    public ModuleBO modulesByName(String name, CheckType checkType) throws SerException {
         Module module = moduleRep.findByName(name);
         StringBuilder sb = new StringBuilder();
-        sb.append("select b.id,b.checkType,a.name from module_table a,(");
+        sb.append("select b.id,b.relation_id as relationId, b.checkType,a.name from module_table a,(");
         sb.append(" select b.id,b.relation_id  ,b.checkType from module_table a ,");
         sb.append(" module_assemble b where a.id = b.module_id and a.name='");
         sb.append(name);
         sb.append("' ");
-        if(null!=checkType){
-            sb.append(" and b.checkType="+checkType.getCode());
+        if (null != checkType) {
+            sb.append(" and b.checkType=" + checkType.getCode());
+        }else {
+            sb.append(" and b.checkType in(0,1)");
         }
-        sb.append(" )b where b.relation_id=a.id");
-        String sql =sb.toString();
-        List<ModuleBO> relations = super.findBySql(sql, ModuleBO.class, new String[]{"id", "checkType", "name"});
+        sb.append(" )b where b.relation_id=a.id order by a.createTime asc");
+        String sql = sb.toString();
+        List<ModuleBO> relations = super.findBySql(sql, ModuleBO.class, new String[]{"id","relationId", "checkType", "name"});
         ModuleBO moduleBO = BeanTransform.copyProperties(module, ModuleBO.class);
         moduleBO.setRelations(relations);
         return moduleBO;
     }
-
 
 
     @Override
@@ -78,19 +83,22 @@ public class ModuleSerImpl extends ServiceImpl<Module, ModuleDTO> implements Mod
         super.remove(id);
     }
 
+    @Transactional
     @Override
-    public void check(String moduleId,String[] relationIds, CheckType checkType) throws SerException {
-        ModuleAssembleDTO dto = new ModuleAssembleDTO();
-        dto.getConditions().add(Restrict.eq("module.id", moduleId));
-        dto.getConditions().add(Restrict.in("relation.id", relationIds));
-        List<ModuleAssemble> moduleAssembles = moduleAssembleSer.findByCis(dto);
-        if (null != moduleAssembles) {
-            for(ModuleAssemble assemble: moduleAssembles){
-                assemble.setCheckType(checkType);
+    public void check(String moduleId, String[] relationIds) throws SerException {
+        String sql = "update module_assemble set checkType=1 where module_id='"+moduleId+"'";
+        super.executeSql(sql);
+        if (null != relationIds && relationIds.length > 0) {
+            String[] tmp = new String[relationIds.length];
+            for(int i=0;i<relationIds.length;i++){
+                tmp[i]="'"+relationIds[i]+"'";
             }
-            moduleAssembleSer.update(moduleAssembles);
+            String relations = StringUtils.join(tmp,",");
+            sql ="update module_assemble set checkType=0 where module_id='"+moduleId+"' and relation_id in("+relations+")";
+            super.executeSql(sql);
         } else {
-            throw new SerException("该数据不存在!");
+            throw new SerException("关联数据不存在!");
         }
+
     }
 }
