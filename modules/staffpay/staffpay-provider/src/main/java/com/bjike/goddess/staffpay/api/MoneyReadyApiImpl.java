675c9ce2fb9e84973b4686de68e6bec3edb4cf59
xiazhili <xiazhili_aj@163.com>
2017-05-20 18:07:04 +0800

    修改代码

--- modules/staffpay/staffpay-provider/src/main/java/com/bjike/goddess/staffpay/api/MoneyReadyApiImpl.java
+++ modules/staffpay/staffpay-provider/src/main/java/com/bjike/goddess/staffpay/api/MoneyReadyApiImpl.java
@@ -1,6 +1,15 @@
 package com.bjike.goddess.staffpay.api;
 
+import com.bjike.goddess.common.api.exception.SerException;
+import com.bjike.goddess.staffpay.bo.CollectCompareBO;
+import com.bjike.goddess.staffpay.bo.MoneyReadyBO;
+import com.bjike.goddess.staffpay.dto.MoneyReadyDTO;
+import com.bjike.goddess.staffpay.service.MoneyReadySer;
+import com.bjike.goddess.staffpay.to.MoneyReadyTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 资金准备审核表业务接口实现
@@ -13,5 +22,43 @@ import org.springframework.stereotype.Service;
  */
 @Service("moneyReadyApiImpl")
 public class MoneyReadyApiImpl implements MoneyReadyAPI {
+
+    @Autowired
+    private MoneyReadySer moneyReadySer;
+
+    @Override
+    public Long countMoneyReady(MoneyReadyDTO moneyReadyDTO) throws SerException {
+        return moneyReadySer.countMoneyReady(moneyReadyDTO);
+    }
+
+    @Override
+    public MoneyReadyBO getOne(String id) throws SerException {
+        return moneyReadySer.getOne(id);
+    }
+
+    @Override
+    public List<MoneyReadyBO> findListMoneyReady(MoneyReadyDTO moneyReadyDTO) throws SerException {
+        return moneyReadySer.findListMoneyReady(moneyReadyDTO);
+    }
+
+    @Override
+    public MoneyReadyBO insertMoneyReady(MoneyReadyTO moneyReadyTO) throws SerException {
+        return moneyReadySer.insertMoneyReady(moneyReadyTO);
+    }
+
+    @Override
+    public MoneyReadyBO editMoneyReady(MoneyReadyTO moneyReadyTO) throws SerException {
+        return moneyReadySer.editMoneyReady(moneyReadyTO);
+    }
+
+    @Override
+    public void removeMoneyReady(String id) throws SerException {
+        moneyReadySer.removeMoneyReady(id);
+    }
+
+    @Override
+    public List<CollectCompareBO> collectCompare(Integer month) throws SerException {
+        return moneyReadySer.collectCompare(month);
+    }
 
 }
