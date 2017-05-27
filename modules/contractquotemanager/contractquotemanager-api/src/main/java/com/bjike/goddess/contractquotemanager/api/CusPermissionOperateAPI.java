1e186a8637e04a9bdb5d8216318ac0d557a7a953
sunfengtao <sunfengtao_aj@163.com>
2017-05-27 18:01:17 +0800

    合同单价

---
+++ modules/contractquotemanager/contractquotemanager-api/src/main/java/com/bjike/goddess/contractquotemanager/api/CusPermissionOperateAPI.java
@@ -0,0 +1,15 @@
+package com.bjike.goddess.contractquotemanager.api;
+
+/**
+ * 客户权限配置操作对象业务接口
+ *
+ * @Author: [ tanghaixiang ]
+ * @Date: [ 2017-05-25 02:12 ]
+ * @Description: [ 客户权限配置操作对象业务接口 ]
+ * @Version: [ v1.0.0 ]
+ * @Copy: [ com.bjike ]
+ */
+public interface CusPermissionOperateAPI {
+
+
+}
