81885847d9c4249e24559c4a14b87db2c5ae522e
sunfengtao <sunfengtao_aj@163.com>
2017-06-05 10:39:53 +0800

    物资购买

---
+++ modules/materialbuy/materialbuy-api/src/main/java/com/bjike/goddess/materialbuy/service/MaterialBuySer.java
@@ -0,0 +1,91 @@
+package com.bjike.goddess.materialbuy.service;
+
+import com.bjike.goddess.common.api.exception.SerException;
+import com.bjike.goddess.common.api.service.Ser;
+import com.bjike.goddess.materialbuy.bo.MaterialBuyBO;
+import com.bjike.goddess.materialbuy.dto.MaterialBuyDTO;
+import com.bjike.goddess.materialbuy.entity.MaterialBuy;
+import com.bjike.goddess.materialbuy.to.MaterialBuyTO;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 物资购买业务接口
+ *
+ * @Author: [ sunfengtao ]
+ * @Date: [ 2017-04-19 04:47 ]
+ * @Description: [  ]
+ * @Version: [ v1.0.0 ]
+ * @Copy: [ com.bjike ]
+ */
+public interface MaterialBuySer extends Ser<MaterialBuy, MaterialBuyDTO> {
+
+    /**
+     * 分页查询物资购买
+     *
+     * @return class MaterialBuyBO
+     * @throws SerException
+     */
+    List<MaterialBuyBO> list(MaterialBuyDTO dto) throws SerException;
+
+    /**
+     * 保存物资购买
+     *
+     * @param to 物资购买to
+     * @return class MaterialBuyBO
+     * @throws SerException
+     */
+    MaterialBuyBO save(MaterialBuyTO to) throws SerException;
+
+    /**
+     * 根据id删除物资购买
+     *
+     * @param id 物资购买唯一标识
+     * @throws SerException
+     */
+    void remove(String id) throws SerException;
+
+    /**
+     * 更新物资购买
+     *
+     * @param to 物资购买to
+     * @throws SerException
+     */
+    void update(MaterialBuyTO to) throws SerException;
+
+    /**
+     * 文件上传
+     *
+     * @param maps 文件名，byte 文件字节
+     * @param path 上传路径
+     */
+    void upload(Map<String, byte[]> maps, String path) throws SerException;
+
+    /**
+     * 查看详情
+     *
+     * @param id 物资购买唯一标识
+     * @return class MaterialBuyBO
+     * @throws SerException
+     */
+    MaterialBuyBO checkDetail(String id) throws SerException;
+
+    /**
+     * 地区负责人审核
+     *
+     * @param to 物资购买to
+     * @throws SerException
+     */
+    void areaPrincipalAudit(MaterialBuyTO to) throws SerException;
+
+    /**
+     * 查询等待付款
+     *
+     * @param dto 物资购买dto
+     * @return class MaterialBuyBO
+     * @throws SerException
+     */
+    List<MaterialBuyBO> findWaitPay(MaterialBuyDTO dto) throws SerException;
+
+}
---
+++ modules/materialbuy/materialbuy-consumer/src/main/java/com/bjike/goddess/materialbuy/action/materialbuy/MaterialBuyAct.java
@@ -0,0 +1,175 @@
+package com.bjike.goddess.materialbuy.action.materialbuy;
+
+import com.bjike.goddess.common.api.exception.ActException;
+import com.bjike.goddess.common.api.exception.SerException;
+import com.bjike.goddess.common.api.restful.Result;
+import com.bjike.goddess.common.consumer.restful.ActResult;
+import com.bjike.goddess.common.utils.bean.BeanTransform;
+import com.bjike.goddess.materialbuy.api.MaterialBuyAPI;
+import com.bjike.goddess.materialbuy.bo.MaterialBuyBO;
+import com.bjike.goddess.materialbuy.dto.MaterialBuyDTO;
+import com.bjike.goddess.materialbuy.to.MaterialBuyTO;
+import com.bjike.goddess.materialbuy.vo.MaterialBuyVO;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 物资购买
+ *
+ * @Author: [ sunfengtao ]
+ * @Date: [ 2017-04-19 04:47 ]
+ * @Description: [  ]
+ * @Version: [ v1.0.0 ]
+ * @Copy: [ com.bjike ]
+ */
+@RestController
+@RequestMapping("materialbuy")
+public class MaterialBuyAct {
+
+    @Autowired
+    private MaterialBuyAPI materialBuyAPI;
+
+    /**
+     * 根据id查询物资购买
+     *
+     * @param id 物资购买唯一标识
+     * @return class MaterialBuyVO
+     * @throws ActException
+     * @version v1
+     */
+    @GetMapping("v1/findbyid/{id}")
+    public Result findById(@PathVariable String id) throws ActException {
+        try {
+            MaterialBuyBO bo = materialBuyAPI.findById(id);
+            MaterialBuyVO vo = BeanTransform.copyProperties(bo, MaterialBuyVO.class);
+            return ActResult.initialize(vo);
+        } catch (SerException e) {
+            throw new ActException(e.getMessage());
+        }
+    }
+
+    /**
+     * 分页查询物资购买
+     *
+     * @param dto 物资购买dto
+     * @return class MaterialBuyVO
+     * @throws ActException
+     * @version v1
+     */
+    @GetMapping("v1/list")
+    public Result list(@Validated MaterialBuyDTO dto, BindingResult bindingResult) throws ActException {
+        try {
+            List<MaterialBuyBO> boList = materialBuyAPI.list(dto);
+            List<MaterialBuyVO> voList = BeanTransform.copyProperties(boList, MaterialBuyVO.class);
+            return ActResult.initialize(voList);
+        } catch (SerException e) {
+            throw new ActException(e.getMessage());
+        }
+    }
+
+    /**
+     * 添加物资购买
+     *
+     * @param to 物资购买to
+     * @return class MaterialBuyVO
+     * @throws ActException
+     * @version v1
+     */
+    @PostMapping("v1/add")
+    public Result add(@Validated({MaterialBuyTO.MaterialBuyAdd.class}) MaterialBuyTO to, BindingResult result) throws ActException {
+        try {
+            MaterialBuyBO bo = materialBuyAPI.save(to);
+            MaterialBuyVO vo = BeanTransform.copyProperties(bo, MaterialBuyVO.class);
+            return ActResult.initialize(vo);
+        } catch (SerException e) {
+            throw new ActException(e.getMessage());
+        }
+    }
+
+    /**
+     * 根据id删除物资购买
+     *
+     * @param id 物资购买唯一标识
+     * @throws ActException
+     * @version v1
+     */
+    @DeleteMapping("v1/delete/{id}")
+    public Result delete(@PathVariable String id) throws ActException {
+        try {
+            materialBuyAPI.remove(id);
+            return new ActResult("delete success!");
+        } catch (SerException e) {
+            throw new ActException(e.getMessage());
+        }
+    }
+
+    /**
+     * 编辑物资购买
+     *
+     * @param to 物资购买to
+     * @throws ActException
+     * @version v1
+     */
+    @PutMapping("v1/edit")
+    public Result edit(@Validated(MaterialBuyTO.MaterialBuyEdit.class) MaterialBuyTO to, BindingResult result) throws ActException {
+        try {
+            materialBuyAPI.update(to);
+            return new ActResult("edit success!");
+        } catch (SerException e) {
+            throw new ActException(e.getMessage());
+        }
+    }
+
+    /**
+     * 查看详情
+     *
+     * @param id 物资购买唯一标识
+     * @return class MaterialBuyVO
+     * @throws SerException
+     * @version v1
+     */
+    @GetMapping("v1/checkdetail/{id}")
+    public Result checkDetail(@PathVariable String id) throws ActException {
+        try {
+            MaterialBuyBO bo = materialBuyAPI.checkDetail(id);
+            MaterialBuyVO vo = BeanTransform.copyProperties(bo, MaterialBuyVO.class);
+            return ActResult.initialize(vo);
+        } catch (SerException e) {
+            throw new ActException(e.getMessage());
+        }
+    }
+
+    /**
+     * 上传文件
+     *
+     * @throws ActException
+     * @version v1
+     */
+    @PutMapping("v1/upload")
+    public Result upload(HttpServletRequest request) throws ActException {
+        return new ActResult("upload success!");
+    }
+
+    /**
+     * 地区负责人审核
+     *
+     * @param to 物资购买to
+     * @throws ActException
+     * @version v1
+     */
+    @PutMapping("v1/areaprincipalaudit")
+    public Result areaPrincipalAudit(@Validated(MaterialBuyTO.PrincipalAudit.class) MaterialBuyTO to, BindingResult result) throws ActException {
+        try {
+            materialBuyAPI.areaPrincipalAudit(to);
+            return new ActResult("areaPrincipalAudit success!");
+        } catch (SerException e) {
+            throw new ActException(e.getMessage());
+        }
+    }
+
+}
