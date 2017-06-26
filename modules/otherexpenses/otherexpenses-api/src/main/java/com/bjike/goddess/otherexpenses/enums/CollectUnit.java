d05154d1d2a95214a9e125b3259b85419bd477ac
邓钧仁 <196329217@qq.com>
2017-06-26 09:34:43 +0800

    增加其他费用模块的权限功能

---
+++ modules/otherexpenses/otherexpenses-api/src/main/java/com/bjike/goddess/otherexpenses/enums/CollectUnit.java
@@ -0,0 +1,39 @@
+package com.bjike.goddess.otherexpenses.enums;
+
+/**
+ * 汇总间隔
+ *
+ * @Author: [zhuangkaiqin]
+ * @Date: [2017-03-16 19:16]
+ * @Description: [汇总间隔]
+ * @Version: [1.0.0]
+ * @Copy: [com.bjike]
+ */
+public enum CollectUnit {
+
+    /**
+     * 每天
+     */
+    EVERYDAY(0),
+    /**
+     * 每周
+     */
+    EVERYWEEK(1),
+    /**
+     * 每月
+     */
+    EVERYMONTH(2),;
+
+    private int code;
+
+    CollectUnit(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+}
+
+
