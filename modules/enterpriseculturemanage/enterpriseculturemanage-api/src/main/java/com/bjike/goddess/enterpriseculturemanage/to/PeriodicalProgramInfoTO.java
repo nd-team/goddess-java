8eb2ef41340d749acbd8adba29b0721f45d6d035
Jason <jason4job@126.com>
2017-07-05 10:07:56 +0800

    企业文化管理修改

--- modules/enterpriseculturemanage/enterpriseculturemanage-api/src/main/java/com/bjike/goddess/enterpriseculturemanage/to/PeriodicalProgramInfoTO.java
+++ modules/enterpriseculturemanage/enterpriseculturemanage-api/src/main/java/com/bjike/goddess/enterpriseculturemanage/to/PeriodicalProgramInfoTO.java
@@ -1,7 +1,12 @@
 package com.bjike.goddess.enterpriseculturemanage.to;
 
+import com.bjike.goddess.common.api.entity.ADD;
+import com.bjike.goddess.common.api.entity.EDIT;
 import com.bjike.goddess.common.api.to.BaseTO;
 import com.bjike.goddess.enterpriseculturemanage.enums.AuditResult;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * 刊物方案信息
@@ -17,62 +22,56 @@ public class PeriodicalProgramInfoTO extends BaseTO {
     /**
      * 方案名称
      */
+    @NotBlank(message = "方案名称不能为空", groups = {ADD.class, EDIT.class})
     private String programName;
-
-    /**
-     * 刊物主题
-     */
-    private String theme;
 
     /**
      * 刊物理念
      */
+    @NotBlank(message = "刊物理念不能为空", groups = {ADD.class, EDIT.class})
     private String ida;
 
     /**
      * 发刊形式
      */
+    @NotBlank(message = "刊物理念不能为空", groups = {ADD.class, EDIT.class})
     private String way;
 
     /**
      * 发刊规格
      */
+    @NotBlank(message = "刊物理念不能为空", groups = {ADD.class, EDIT.class})
     private String standard;
 
     /**
      * 执笔人
      */
+    @NotBlank(message = "刊物理念不能为空", groups = {ADD.class, EDIT.class})
     private String executer;
 
     /**
      * 刊物名称
      */
+    @NotBlank(message = "刊物理念不能为空", groups = {ADD.class, EDIT.class})
     private String periodicalName;
 
     /**
      * 发刊时间
      */
+    @NotBlank(message = "发刊时间不能为空", groups = {ADD.class, EDIT.class})
     private String time;
 
     /**
      * 发刊费用
      */
+    @NotNull(message = "发刊费用不能为空", groups = {ADD.class, EDIT.class})
     private Double cost;
 
     /**
      * 发刊周期
      */
+    @NotBlank(message = "发刊周期不能为空", groups = {ADD.class, EDIT.class})
     private String cycle;
-
-    /**
-     * 审核结果
-     */
-    private AuditResult auditResult;
-
-    /**
-     * 审核意见
-     */
-    private String auditSuggestion;
 
     /**
      * 企业文化信息id
@@ -85,14 +84,6 @@ public class PeriodicalProgramInfoTO extends BaseTO {
 
     public void setProgramName(String programName) {
         this.programName = programName;
-    }
-
-    public String getTheme() {
-        return theme;
-    }
-
-    public void setTheme(String theme) {
-        this.theme = theme;
     }
 
     public String getIda() {
@@ -157,22 +148,6 @@ public class PeriodicalProgramInfoTO extends BaseTO {
 
     public void setCycle(String cycle) {
         this.cycle = cycle;
-    }
-
-    public AuditResult getAuditResult() {
-        return auditResult;
-    }
-
-    public void setAuditResult(AuditResult auditResult) {
-        this.auditResult = auditResult;
-    }
-
-    public String getAuditSuggestion() {
-        return auditSuggestion;
-    }
-
-    public void setAuditSuggestion(String auditSuggestion) {
-        this.auditSuggestion = auditSuggestion;
     }
 
     public String getInfoId() {
