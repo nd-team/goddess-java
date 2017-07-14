a1367746e5b379265d1c99e5abf6ea4814c81b91
邓钧仁 <196329217@qq.com>
2017-07-14 18:53:41 +0800

    修改出车ConditionTO.java

--- modules/dispatchcar/dispatchcar-api/src/main/java/com/bjike/goddess/dispatchcar/to/ConditionTO.java
+++ modules/dispatchcar/dispatchcar-api/src/main/java/com/bjike/goddess/dispatchcar/to/ConditionTO.java
@@ -35,6 +35,9 @@ public class ConditionTO extends BaseTO {
      */
     private LocalDate[] dispatchDate;
 
+    private String DispatchStartDate;
+    private String DispatchEndDate;
+
     public String getArea() {
         return area;
     }
@@ -65,5 +68,21 @@ public class ConditionTO extends BaseTO {
 
     public void setDispatchDate(LocalDate[] dispatchDate) {
         this.dispatchDate = dispatchDate;
+    }
+
+    public String getDispatchStartDate() {
+        return DispatchStartDate;
+    }
+
+    public void setDispatchStartDate(String dispatchStartDate) {
+        DispatchStartDate = dispatchStartDate;
+    }
+
+    public String getDispatchEndDate() {
+        return DispatchEndDate;
+    }
+
+    public void setDispatchEndDate(String dispatchEndDate) {
+        DispatchEndDate = dispatchEndDate;
     }
 }
