0d630507be4a6fa0a3841779d4154157aeec951f
xiazhili <xiazhili_aj@163.com>
2017-07-18 15:36:26 +0800

    水电费缴付添加权限

---
+++ modules/rentutilitiespay/rentutilitiespay-api/src/main/java/com/bjike/goddess/rentutilitiespay/bo/CusOperateBO.java
@@ -0,0 +1,29 @@
+package com.bjike.goddess.rentutilitiespay.bo;
+
+import com.bjike.goddess.common.api.bo.BaseBO;
+
+/**
+ * 权限配置业务传输对象list返回对象
+ *
+ * @Author: [ xiazhili ]
+ * @Date: [ 2017-05-12 05:43 ]
+ * @Description: [ 权限配置业务传输对象list返回对象 ]
+ * @Version: [ v1.0.0 ]
+ * @Copy: [ com.bjike ]
+ */
+public class CusOperateBO extends BaseBO {
+
+
+    /**
+     * 操作对象
+     */
+    private String operator;
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+}
