882167c5911943277c274ce68b0a283bbd37c04c
thx <804322698@qq.com>
2017-07-31 11:11:30 +0800

    修改附件上传大小

--- modules/carinfo/carinfo-consumer/src/main/java/com/bjike/goddess/carinfo/Application.java
+++ modules/carinfo/carinfo-consumer/src/main/java/com/bjike/goddess/carinfo/Application.java
@@ -14,6 +14,14 @@ import java.io.IOException;
 @ImportResource({"classpath:app.xml"})
 public class Application {
 
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize("800MB");
+		factory.setMaxRequestSize("800MB");
+		return factory.createMultipartConfig();
+	}
+
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Application.class,args);
--- modules/contacts/contacts-consumer/src/main/java/com/bjike/goddess/contacts/Application.java
+++ modules/contacts/contacts-consumer/src/main/java/com/bjike/goddess/contacts/Application.java
@@ -2,8 +2,10 @@ package com.bjike.goddess.contacts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.*;
 
+import javax.servlet.MultipartConfigElement;
 import java.io.IOException;
 
 /**
@@ -17,6 +19,14 @@ import java.io.IOException;
 @PropertySource(value = {"classpath:permission.properties"},encoding="utf-8")
 @ImportResource("classpath:app.xml")
 public class Application {
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("800MB");
+        factory.setMaxRequestSize("800MB");
+        return factory.createMultipartConfig();
+    }
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(Application.class, args);
