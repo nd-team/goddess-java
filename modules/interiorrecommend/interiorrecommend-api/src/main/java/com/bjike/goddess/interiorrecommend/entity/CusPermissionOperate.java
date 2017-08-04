b162bf4bccb8fa2a6d9e37eccd53fcbdc79beb91
MissJiang <978415632@qq.com>
2017-08-04 20:59:41 +0800

    内部推荐bug修复

--- modules/interiorrecommend/interiorrecommend-api/src/main/java/com/bjike/goddess/interiorrecommend/entity/CusPermissionOperate.java
+++ modules/interiorrecommend/interiorrecommend-api/src/main/java/com/bjike/goddess/interiorrecommend/entity/CusPermissionOperate.java
@@ -17,7 +17,7 @@ import javax.persistence.Table;
  * @Copy: [ com.bjike ]
  */
 @Entity
-@Table(name = "businessproject_cuspermissionoperate")
+@Table(name = "interiorrecommend_cuspermissionoperate")
 public class CusPermissionOperate extends BaseEntity {
 
 
