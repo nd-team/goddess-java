3473059996bd389a0dbe1f54259a72617a3b9174
xiazhili <xiazhili_aj@163.com>
2017-08-23 09:15:20 +0800

    萨芬

---
+++ modules/managepromotion/managepromotion-api/src/main/java/com/bjike/goddess/managepromotion/vo/LittleVO.java
@@ -0,0 +1,25 @@
+package com.bjike.goddess.managepromotion.vo;
+
+/**
+ * Created by ike on 17-8-21.
+ */
+public class LittleVO {
+    private String major;
+    private String grade;
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
