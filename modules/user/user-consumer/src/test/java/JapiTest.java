bd0162a718f39134b98cde0b4faba4274e3bd8d0
chenyang <1663651444@qq.com>
2018-01-10 16:43:12 +0800

    api路径修改

--- modules/user/user-consumer/src/test/java/JapiTest.java
+++ modules/user/user-consumer/src/test/java/JapiTest.java
@@ -3,12 +3,6 @@ import com.dounine.japi.JapiClientStorage;
 import com.dounine.japi.JapiClientTransfer;
 import com.dounine.japi.core.IProject;
 import com.dounine.japi.core.impl.ProjectImpl;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.ImportResource;
 
 import java.io.IOException;
 
@@ -16,7 +10,7 @@ public class JapiTest {
 	public static void main(String[] args) throws IOException {
 //		JapiClient.delete(true);
 		JapiClient.setClassLoader(JapiTest.class);
-		JapiClient.setPrefixPath("/home/ike/github/ljt/goddess-java/");//路径前缀
+		JapiClient.setPrefixPath("/home/ike/github/goddess-java/");//路径前缀
 		JapiClient.setpostfixPath("/src/main/java");
 		JapiClient.setProjectJavaPath("modules/user/user-consumer");//主项目位置
 		JapiClient.setActionReletivePath("com/bjike/goddess/user/action");//主项目action位置
